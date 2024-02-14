4441e2045e9e9f36bd0cc32db52ae02058940332
CarlosDiaz <carlosdiaz0912000@gmail.com>
2024-02-14 00:42:22 -0500

    se termino ej m6 sin tres ej pertenecientes a arrays y se soluciono hasta el ej 9 del m7 ejs 4 y 5 ptes

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,11 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+var temp = 1;
+for (var i = a; i <= b; i++){
+  temp = temp * i;
+}
+return Math.abs(temp);
 }
 
 module.exports = productoEntreNúmeros;
